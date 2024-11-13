9b33f55d25e4a1b4a97af7ecb88b4df0d16f4e00
thoraho <thoraho@hiof.no>
2024-11-13 13:26:25 +0100

    Pushepush - rydder opp

--- oppgave_1/frontend/src/components/Lesson.tsx
+++ oppgave_1/frontend/src/components/Lesson.tsx
@@ -1,200 +1,164 @@
-// "use client";
+"use client";
 
-// import { comments, courses } from "@/data/data";
-// import { useCourse } from "@/hooks/useCourse";
-// // import {
-// //     categories,
-// //     comments,
-// //     courseCreateSteps,
-// //     courses,
-// //     users,
-// //   } from "../data/data";
+import { comments, courses } from "@/data/data";
+import { useCourse } from "@/hooks/useCourse";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { CommentType, CourseType, LessonType } from "@/components/types";
 
-// import { useParams, useRouter } from "next/navigation";
-// import { useEffect, useState } from "react";
-// // import {
-// //     categories,
-// //     comments,
-// //     courseCreateSteps,
-// //     courses,
-// //     users,
-// //   } from "../data/data";
+export default function Lesson() {
+  const [success, setSuccess] = useState(false);
+  const [formError, setFormError] = useState(false);
+  const [comment, setComment] = useState("");
+  const [name, setName] = useState("");
+  const [lessonComments, setComments] = useState<CommentType[]>([]);
+  const [lesson, setLesson] = useState<LessonType | null>(null);
+  const [course, setCourse] = useState<CourseType | null>(null);
+  const courseSlug = "javascript-101";
+  const lessonSlug = "variabler";
 
-// const getComments = async (lessonSlug: any) => {
-//     const data = await comments.filter(
-//         (comment) => comment.lesson.slug === lessonSlug
-//     );
-//     return data;
-// };
+  const handleComment = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(event.target.value);
+  };
 
-// const createComment = async (data: any) => {
-//     await comments.push(data);
-// };
+  const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
 
-// export default function Lesson() {
-//     const {getCourse} = useCourse()
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setFormError(false);
+    setSuccess(false);
+    if (!comment || !name) {
+      setFormError(true);
+    } else {
+      await createComment({
+        id: `${Math.floor(Math.random() * 1000 + 1)}`,
+        createdBy: {
+          id: Math.floor(Math.random() * 1000 + 1),
+          name,
+        },
+        comment,
+        lesson: { slug: lessonSlug },
+      });
+      const commentsData = await getComments(lessonSlug);
+      setComments(commentsData);
+      setSuccess(true);
+    }
+  };
 
-//     const [success, setSuccess] = useState(false);
-//     const [formError, setFormError] = useState(false);
-//     const [lessonComments, setComments] = useState([]);
-//     const [comment, setComment] = useState("");
-//     const [name, setName] = useState("");
-//     const [lesson, setLesson] = useState(null);
-//     const [course, setCourse] = useState(null);
-//     const courseSlug = "javascript-101";
-//     const lessonSlug = "variabler";
+  useEffect(() => {
+    const getContent = async () => {
+      const lessonDate = await getLesson(courseSlug, lessonSlug);
+      const courseData = await getCourse(courseSlug);
+      const commentsData = await getComments(lessonSlug);
 
-//     const getLesson = async (courseSlug: any, lessonSlug: any) => {
-//         const data = await courses.flatMap((course) =>
-//                 course.slug === courseSlug &&
-//                 course.lessons.filter((lesson: any) => lesson.slug === lessonSlug)
-//             )
-//             .filter(Boolean);
-//         return data?.[0];
-//     };
+      setLesson(lessonDate ?? null);
+      setCourse(courseData);
+      setComments(commentsData);
+    };
+  }, [courseSlug, lessonSlug]);
 
-//     const handleComment = (event: any) => {
-//         setComment(event.target.value);
-//     };
-
-//     const handleName = (event: any) => {
-//         setName(event.target.value);
-//     };
-
-//     const handleSubmit = async (event: any) => {
-//         event.preventDefault();
-//         setFormError(false);
-//         setSuccess(false);
-//         if (!comment || !name) {
-//             setFormError(true);
-//         } else {
-//             await createComment({
-//                 id: `${Math.floor(Math.random() * 1000 + 1)}`,
-//                 createdBy: {
-//                     id: Math.floor(Math.random() * 1000 + 1),
-//                     name,
-//                 },
-//                 comment,
-//                 lesson: { slug: lessonSlug },
-//             });
-//             const commentsData = await getComments(lessonSlug);
-//             setComments(commentsData);
-//             setSuccess(true);
-//         }
-//     };
-
-//     useEffect(() => {
-//         const getContent = async () => {
-//             const lessonDate = await getLesson(courseSlug, lessonSlug);
-//             const courseData = await getCourse(courseSlug, lessonSlug);
-//             const commentsData = await getComments(lessonSlug);
-//             setLesson(lessonDate);
-//             setCourse(courseData);
-//             setComments(commentsData);
-//         };
-//         getContent();
-//     }, [courseSlug, lessonSlug]);
-
-//     return (
-//         <div>
-//             <div className="flex justify-between">
-//                 <h3 data-testid="course_title" className="mb-6 text-base font-bold">
-//                     <a className="underline" href={`/kurs/${course?.slug}`}>
-//                         {course?.title}
-//                     </a>
-//                 </h3>
-//                 <span data-testid="course_category">
-//                     Kategori: <span className="font-bold">{course?.category}</span>
-//                 </span>
-//             </div>
-//             <h2 className="text-2xl font-bold" data-testid="lesson_title">
-//                 {lesson?.title}
-//             </h2>
-//             <p
-//                 data-testid="lesson_preAmble"
-//                 className="mt-4 font-semibold leading-relaxed"
-//             >
-//                 {lesson?.preAmble}
-//             </p>
-//             {lesson?.text?.length > 0 &&
-//                 lesson.text.map((text: any) => (
-//                     <p
-//                         data-testid="lesson_text"
-//                         className="mt-4 font-normal"
-//                         key={text.id}
-//                     >
-//                         {text.text}
-//                     </p>
-//                 ))}
-//             <section data-testid="comments">
-//                 <h4 className="mt-8 mb-4 text-lg font-bold">
-//                     Kommentarer ({lessonComments?.length})
-//                 </h4>
-//                 <form data-testid="comment_form" onSubmit={handleSubmit} noValidate>
-//                     <label className="mb-4 flex flex-col" htmlFor="name">
-//                         <span className="mb-1 text-sm font-semibold">Navn*</span>
-//                         <input
-//                             data-testid="form_name"
-//                             type="text"
-//                             name="name"
-//                             id="name"
-//                             value={name}
-//                             onChange={handleName}
-//                             className="w-full rounded bg-slate-100"
-//                         />
-//                     </label>
-//                     <label className="mb-4 flex flex-col" htmlFor="comment">
-//                         <span className="mb-1 text-sm font-semibold">
-//                             Legg til kommentar*
-//                         </span>
-//                         <textarea
-//                             data-testid="form_textarea"
-//                             type="text"
-//                             name="comment"
-//                             id="comment"
-//                             value={comment}
-//                             onChange={handleComment}
-//                             className="w-full rounded bg-slate-100"
-//                             cols="30"
-//                         />
-//                     </label>
-//                     <button
-//                         className="rounded bg-emerald-600 px-10 py-2 text-center text-base text-white"
-//                         data-testid="form_submit"
-//                         type="submit"
-//                     >
-//                         Legg til kommentar
-//                     </button>
-//                     {formError ? (
-//                         <p className="font-semibold text-red-500" data-testid="form_error">
-//                             Fyll ut alle felter med *
-//                         </p>
-//                     ) : null}
-//                     {success ? (
-//                         <p
-//                             className="font-semibold text-emerald-500"
-//                             data-testid="form_success"
-//                         >
-//                             Skjema sendt
-//                         </p>
-//                     ) : null}
-//                 </form>
-//                 <ul className="mt-8" data-testid="comments_list">
-//                     {lessonComments?.length > 0
-//                         ? lessonComments.map((c) => (
-//                             <li
-//                                 className="mb-6 rounded border border-slate-200 px-4 py-6"
-//                                 key={c.id}
-//                             >
-//                                 <h5 data-testid="user_comment_name" className="font-bold">
-//                                     {c.createdBy.name}
-//                                 </h5>
-//                                 <p data-testid="user_comment">{c.comment}</p>
-//                             </li>
-//                         ))
-//                         : null}
-//                 </ul>
-//             </section>
-//         </div>
-//     );
-// }
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h3 data-testid="course_title" className="mb-6 text-base font-bold">
+          <a className="underline" href={`/kurs/${course?.slug}`}>
+            {course?.title}
+          </a>
+        </h3>
+        <span data-testid="course_category">
+          Kategori: <span className="font-bold">{course?.category}</span>
+        </span>
+      </div>
+      <h2 className="text-2xl font-bold" data-testid="lesson_title">
+        {lesson?.title}
+      </h2>
+      <p
+        data-testid="lesson_preAmble"
+        className="mt-4 font-semibold leading-relaxed"
+      >
+        {lesson?.preAmble}
+      </p>
+      {(lesson?.text ?? []).length > 0 &&
+        lesson?.text.map((text) => (
+          <p
+            data-testid="lesson_text"
+            className="mt-4 font-normal"
+            key={text.id}
+          >
+            {text.text}
+          </p>
+        ))}
+      <section data-testid="comments">
+        <h4 className="mt-8 mb-4 text-lg font-bold">
+          Kommentarer ({lessonComments?.length})
+        </h4>
+        <form data-testid="comment_form" onSubmit={handleSubmit} noValidate>
+          <label className="mb-4 flex flex-col" htmlFor="name">
+            <span className="mb-1 text-sm font-semibold">Navn*</span>
+            <input
+              data-testid="form_name"
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={handleName}
+              className="w-full rounded bg-slate-100"
+            />
+          </label>
+          <label className="mb-4 flex flex-col" htmlFor="comment">
+            <span className="mb-1 text-sm font-semibold">
+              Legg til kommentar*
+            </span>
+            <textarea
+              data-testid="form_textarea"
+              //   type="text"
+              name="comment"
+              id="comment"
+              value={comment}
+              onChange={handleComment}
+              className="w-full rounded bg-slate-100"
+              cols={30}
+            />
+          </label>
+          <button
+            className="rounded bg-emerald-600 px-10 py-2 text-center text-base text-white"
+            data-testid="form_submit"
+            type="submit"
+          >
+            Legg til kommentar
+          </button>
+          {formError ? (
+            <p className="font-semibold text-red-500" data-testid="form_error">
+              Fyll ut alle felter med *
+            </p>
+          ) : null}
+          {success ? (
+            <p
+              className="font-semibold text-emerald-500"
+              data-testid="form_success"
+            >
+              Skjema sendt
+            </p>
+          ) : null}
+        </form>
+        <ul className="mt-8" data-testid="comments_list">
+          {lessonComments?.length > 0
+            ? lessonComments.map((c) => (
+                <li
+                  className="mb-6 rounded border border-slate-200 px-4 py-6"
+                  key={c.id}
+                >
+                  <h5 data-testid="user_comment_name" className="font-bold">
+                    {c.createdBy.name}
+                  </h5>
+                  <p data-testid="user_comment">{c.comment}</p>
+                </li>
+              ))
+            : null}
+        </ul>
+      </section>
+    </div>
+  );
+}
