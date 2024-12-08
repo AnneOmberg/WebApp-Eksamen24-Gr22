61c21568081bf6ed5e21bd320c926a4ffdbabb33
Vilped1 <vildeandrea01@gmail.com>
2024-12-08 18:20:11 +0100

    IDK

---
+++ oppgave_2/backend/prisma/migrations/20241208140955_price_is_string/migration.sql
@@ -0,0 +1,22 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Template" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "allowSameDay" BOOLEAN NOT NULL DEFAULT true,
+    "allowedWeekdays" TEXT NOT NULL,
+    "isPrivate" BOOLEAN NOT NULL DEFAULT false,
+    "hasLimitedSeats" BOOLEAN NOT NULL DEFAULT false,
+    "seatLimit" INTEGER,
+    "hasFixedPrice" BOOLEAN NOT NULL DEFAULT false,
+    "price" TEXT,
+    "isFree" BOOLEAN NOT NULL DEFAULT false,
+    "hasWaitlist" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_Template" ("allowSameDay", "allowedWeekdays", "description", "hasFixedPrice", "hasLimitedSeats", "hasWaitlist", "id", "isFree", "isPrivate", "price", "seatLimit", "title") SELECT "allowSameDay", "allowedWeekdays", "description", "hasFixedPrice", "hasLimitedSeats", "hasWaitlist", "id", "isFree", "isPrivate", "price", "seatLimit", "title" FROM "Template";
+DROP TABLE "Template";
+ALTER TABLE "new_Template" RENAME TO "Template";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
