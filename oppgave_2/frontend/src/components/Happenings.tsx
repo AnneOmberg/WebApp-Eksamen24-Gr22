816cbb93913d16083be6b4535a6ed8ffc274e850
thoraho <thoraho@hiof.no>
2024-12-09 10:51:04 +0100

    alksjd

--- oppgave_2/frontend/src/components/Happenings.tsx
+++ oppgave_2/frontend/src/components/Happenings.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import useHappening from "@/hooks/useHappening";
 import { useEffect, useState } from "react";
@@ -11,9 +11,14 @@ import { usePathname } from "next/navigation";
 // }
 
 export default function Happenings() {
-  const { categories, happenings, setHappenings, deleteHappening } = useHappening()
+  const { categories, happenings, setHappenings, deleteHappening } =
+    useHappening();
+  console.log("Happenings:", happenings); // Log happenings
   const [value, setValue] = useState<string>("");
-  const [filteredHappenings, setFilteredHappenings] = useState<HappeningType[]>([]);
+  const [filteredHappenings, setFilteredHappenings] = useState<HappeningType[]>(
+    []
+  );
+  console.log("Filtered happenings:", filteredHappenings); // Log filtered happenings
   const [categoryFilter, setCategoryFilter] = useState<string>("");
 
   const pathname = usePathname();
@@ -28,12 +33,12 @@ export default function Happenings() {
     const category = event.target.value;
     setValue(category);
 
-    console.log('Handling filter, category:', category); // Log category
+    console.log("Handling filter, category:", category); // Log category
     if (category) {
       const filtered = happenings?.filter((hap) =>
         hap?.category?.toLowerCase()?.includes(category?.toLowerCase())
       );
-      console.log('Filtered happenings:', filtered); // Log filtered array
+      console.log("Filtered happenings:", filtered); // Log filtered array
       setFilteredHappenings(filtered);
     } else if (!category) {
       setFilteredHappenings(happenings);
@@ -44,12 +49,16 @@ export default function Happenings() {
     const filterValue = event.target.value;
     setValue(filterValue);
     let filtered = [...happenings];
-  
+
     if (filterValue === "date_asc") {
-      const sorted = filtered.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+      const sorted = filtered.sort(
+        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+      );
       setFilteredHappenings(sorted);
     } else if (filterValue === "date_desc") {
-      const sorted = filtered.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+      const sorted = filtered.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
       setFilteredHappenings(sorted);
     } else if (filterValue === "alphabetical") {
       const sorted = filtered.sort((a, b) => a.title.localeCompare(b.title));
@@ -64,7 +73,7 @@ export default function Happenings() {
   return (
     <>
       <header className="mt-8 flex items-center justify-between">
-      <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold">
           {isAdmin ? "Admin - Arrangementer" : "Finn nytt arrangement"}
         </h1>
         <label className="flex flex-col text-xs font-semibold" htmlFor="filter">
@@ -78,7 +87,9 @@ export default function Happenings() {
           >
             <option value="">Alle</option>
             {categories.map((category) => (
-              <option key={category} value={category}>{category}</option>
+              <option key={category} value={category}>
+                {category}
+              </option>
             ))}
           </select>
         </label>
@@ -99,11 +110,20 @@ export default function Happenings() {
         </label>
       </header>
 
-      <section className={`flex flex-wrap m-5 ${filteredHappenings.length > 1 && filteredHappenings.length % 2 === 1 ? "justify-start" : "justify-center"}`} data-testid="courses">
-        {filteredHappenings && filteredHappenings.length > 0 ? (
+      <section
+        className={`flex flex-wrap m-5 ${
+          filteredHappenings.length > 1 && filteredHappenings.length % 2 === 1
+            ? "justify-start"
+            : "justify-center"
+        }`}
+        data-testid="courses"
+      >
+        {filteredHappenings ? (
           filteredHappenings.map((hap, i) => (
             <article
-              className={`w-5/12 min-h-max m-4 border-2 border-gray-300 rounded-lg shadow-lg bg-white p-4 ${i === filteredHappenings.length - 1 ? "last-item-style" : ""}`}
+              className={`w-5/12 min-h-max m-4 border-2 border-gray-300 rounded-lg shadow-lg bg-white p-4 ${
+                i === filteredHappenings.length - 1 ? "last-item-style" : ""
+              }`}
               key={hap.id}
               data-testid="course_wrapper"
             >
@@ -112,7 +132,13 @@ export default function Happenings() {
                   className="font-bold text-2xl mb-2"
                   data-testid="courses_title"
                 >
-                  <Link href={isAdmin ? `/admin/Happenings/${hap.slug}` : `/Happenings/${hap.slug}`}>
+                  <Link
+                    href={
+                      isAdmin
+                        ? `/admin/Happenings/${hap.slug}`
+                        : `/Happenings/${hap.slug}`
+                    }
+                  >
                     {hap.title}
                   </Link>
                 </h3>
@@ -121,41 +147,46 @@ export default function Happenings() {
                 </span>
               </section>
               <section className="p-4">
-              <ul className="mb-4">
-                <li className="text-gray-700 mb-1">{hap.description}</li>
-                <li className="text-gray-700 mb-1">
-                  {new Date(hap.date).toLocaleDateString("no-NO", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  })}
-                </li>
-                <li className="text-gray-700 mb-1">{hap.location}</li>
-                <li className="text-gray-700 mb-1">{hap.price} kr</li>
-                <li className="text-gray-700 mb-1">{hap.seats} plasser</li>
-              </ul>
-              <div className="flex justify-between items-center">
-              {!isAdmin && (
-                <button className="border-2 bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-600">
-                <Link className="font-semibold underline" href={`/Happenings/${hap.slug}/order`}>Kjøp biletter</Link>
-                </button>
-                )}
-                {isAdmin && (
-                  <button
-                    onClick={() => deleteHappening(hap.id)}
-                    className="px-4 py-2 rounded bg-red-600 text-white hover:bg-red-700"
-                  >
-                    Slett
-                  </button>
-                )}
-                <Link
+                <ul className="mb-4">
+                  <li className="text-gray-700 mb-1">{hap.description}</li>
+                  <li className="text-gray-700 mb-1">
+                    {new Date(hap.date).toLocaleDateString("no-NO", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </li>
+                  <li className="text-gray-700 mb-1">{hap.location}</li>
+                  <li className="text-gray-700 mb-1">{hap.price} kr</li>
+                  <li className="text-gray-700 mb-1">{hap.seats} plasser</li>
+                </ul>
+                <div className="flex justify-between items-center">
+                  {!isAdmin && (
+                    <button className="border-2 bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-600">
+                      <Link
+                        className="font-semibold underline"
+                        href={`/Happenings/${hap.slug}/order`}
+                      >
+                        Kjøp biletter
+                      </Link>
+                    </button>
+                  )}
+                  {isAdmin && (
+                    <button
+                      onClick={() => deleteHappening(hap.id)}
+                      className="px-4 py-2 rounded bg-red-600 text-white hover:bg-red-700"
+                    >
+                      Slett
+                    </button>
+                  )}
+                  <Link
                     className="text-blue-500 hover:underline"
                     data-testid="courses_url"
                     href={`/Happenings/${hap.slug}`}
                   >
-                  Info
+                    Info
                   </Link>
-              </div>
+                </div>
               </section>
             </article>
           ))
@@ -164,5 +195,5 @@ export default function Happenings() {
         )}
       </section>
     </>
-  )
+  );
 }
